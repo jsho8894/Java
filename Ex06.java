3ecf7f034a62ae27e7b56237c145cba13e0b8930
jsho8894 <61223191+jsho8894@users.noreply.github.com>
2020-05-07 18:07:34 +0900

    Create Ex06.java

---
+++ Ex06.java
@@ -0,0 +1,12 @@
+class Ex06{
+	public static void main(String[] args){
+		byte a;
+		a = 127;
+		a++;
+		System.out.println("a=" +a);
+		int b;
+		b=321;
+		a=(byte)b;
+		System.out.println("a="+a);
+	}
+}
