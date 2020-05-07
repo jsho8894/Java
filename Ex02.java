2ded4f76b0bca6008a633e327ad508f73ece8573
jsho8894 <61223191+jsho8894@users.noreply.github.com>
2020-05-07 13:08:42 +0900

    Update Ex02.java

--- Ex02.java
+++ Ex02.java
@@ -1,7 +1,11 @@
 class Ex02{
 	public static void main(String[] args){
-		int a;
-		a=12;		
-		System.out.println(a);
+
+		System.out.println('A'-'A');
+		System.out.println('B'-'A');
+		System.out.println('C'-'A');
+		System.out.println('a'-'A');
+		System.out.println(2147483647);
+		System.out.println(-2147483648);
 	}
 }
